3c94e5cbe21b8409b2917bfd283bf4474685b7c7
SHAURYA KUSHWAH <103944673+ShauryaK0303@users.noreply.github.com>
2023-12-03 09:58:16 +0530

    Update vehicle_app.py

--- vehicle_app.py
+++ vehicle_app.py
@@ -1,14 +1,11 @@
-# app.py
-
 import streamlit as st
 import pandas as pd
 import pickle
 
-# Load the pre-trained model
+# Load the trained model
 with open('final_model1.pkl', 'rb') as model_file:
     model = pickle.load(model_file)
 
-# Streamlit app
 def main():
     st.set_page_config(page_title="Vehicle Insurance Prediction", page_icon="🚗")
 
@@ -24,7 +21,6 @@ def main():
     vehicle_damage = st.sidebar.radio("Vehicle Damage in the Past", ["Yes", "No"])
     vehicle_age = st.sidebar.radio("Vehicle Age", ["< 1 Year", "1-2 Year", " >2 Years"])
 
-    # Map user input to numerical values
     gender_mapping = {"Male": 0, "Female": 1}
     previously_insured_mapping = {"Yes": 1, "No": 0}
     vehicle_damage_mapping = {"Yes": 0, "No": 1}
@@ -35,17 +31,14 @@ def main():
     vehicle_damage = vehicle_damage_mapping[vehicle_damage]
     vehicle_age = vehicle_age_mapping[vehicle_age]
 
-    # Make a prediction
     user_data = [[gender, age, previously_insured, vehicle_damage, vehicle_age]]
     prediction = model.predict(user_data)
 
-    # Display the result
     st.markdown("---")
     st.subheader("Prediction Result:")
     result = "Interested in vehicle insurance." if prediction[0] == 1 else "Not interested in vehicle insurance."
     st.success(result)
 
-    # Footer
     st.markdown("---")
     st.write(
         "Developed by: SHAURYA KUSHWAH\n"
